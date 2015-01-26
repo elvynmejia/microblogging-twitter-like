658d73b13c57bc8737e918c0166a47ea555397d4
elvynmejia <elvynmejia@gmail.com>
2015-01-25 21:44:25 -0800

    Finish user signup, use SSL and Unicorn in production

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,33 @@
 class UsersController < ApplicationController
 	#for creating new users
+  #controller convention of plural resource name
   def new
+  	@user = User.new
   end
+
+  def show
+  	@user = User.find(params[:id])
+  	#debugger
+  end 
+
+  def create
+	@user = User.new(user_params)
+  	if @user.save
+  		#handle a successful save
+  		#render the profile
+      #not entirely sure how this renders the new created user profile
+      #there is no view that corresponds to the newly created user profile
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+  	else
+  		#handle a failure save
+  		render 'new'
+  	end 
+  end 
+
+  private
+  	def user_params
+  		params.require(:user).permit(:name, :email, :password, 
+  									 :password_confirmation)
+  	end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
- # get 'users/new'
-
   #Define named routes
   root             'static_pages#home'
   get 'help'    => 'static_pages#help'
@@ -8,6 +6,8 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
 
   get 'signup'  => 'users#new'
+  
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
