9c5e5909b05386ccaf8aaca3579807610457d818
elvynmejia <elvynmejia@gmail.com>
2015-07-28 14:39:56 -0700

    Try to diagnose account activation email error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,16 +64,16 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<usersmicropostsapp>.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host}
+  host = 'usersmicropostsapp.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
-    :authentcation  => :plain,
+    :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
-    :enable_starttls_auto => true      
+    :enable_starttls_auto => true
   }
 
 
