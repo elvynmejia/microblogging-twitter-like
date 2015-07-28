06c17dd83d9c6180a0105211d87ca7d406134869
elvynmejia <elvynmejia@gmail.com>
2015-07-28 13:09:18 -0700

    Add password resets & email configuration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,13 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.paginate(page: params[:page])
+   # @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end 
 
   def show
   	@user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
   	#debugger
   end 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   #Define named routes
   root             'static_pages#home'
   get     'help'    => 'static_pages#help'
@@ -11,4 +15,5 @@ Rails.application.routes.draw do
   
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
