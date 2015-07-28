c7631b21743c85c0a543fc2dad90557419de907b
elvynmejia <elvynmejia@gmail.com>
2015-07-28 14:57:40 -0700

    Fix merge conflict

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,11 +65,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'usersmicropostsapp.herokuapp.com'
-<<<<<<< HEAD
   config.action_mailer.default_url_options = { host: host}
-=======
-  config.action_mailer.default_url_options = { host: host }
->>>>>>> account-activation-password-reset
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
